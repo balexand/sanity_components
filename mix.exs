e1a06cfd8f16faad1864ccf4d160586a53f0760a
Brian Alexander <balexand@gmail.com>
2022-03-16 19:34:56 -0600

    CI (#1)
    * CI
    
    * CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SanityComponents.MixProject do
     [
       app: :sanity_components,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,9 @@ defmodule SanityComponents.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # dev/test
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
